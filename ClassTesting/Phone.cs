5538618e4206c17f6dde9c839e651f6dbaacf817
egork <egork@DESKTOP-QBJBKM0>
2021-05-16 17:25:18 +0300

    End exercise 1

---
+++ ClassTesting/Phone.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTesting
+{
+
+
+    class Phone
+    {
+        public string number;
+        public string model;
+        public int weight;
+        public string name;
+
+        public Phone() {
+            this.number = "";
+            this.model = "";
+            this.weight = 0;
+        }
+
+        public Phone(string number, string model) {
+            this.number = number;
+            this.model = model;
+        }
+
+        public Phone(string number, string model, int weight):this(number,model) {
+            this.weight = weight;
+        }
+
+        public void dataNumber()
+        {
+            Console.WriteLine("number: {0}, model: {1}, weight: {2}", this.number, this.model, this.weight);
+        }
+
+        public void recieveCall(Phone userName) {
+            Console.WriteLine($"Звонит {userName.name}");
+        }
+        public void recieveCall(string name, string number)
+        {
+            Console.WriteLine($"Звонит {name}, с номером: {number}");
+        }
+
+        public string getNumber(Phone userNumber) {
+            return userNumber.number;
+        }
+
+        public void sendMessage(params string[] nums)
+        {
+            StringBuilder res = new StringBuilder();
+            foreach (var item in nums)
+            {
+                res.Append(item + ", ");
+            }
+            res.Remove(res.Length-2, 1);
+
+            Console.WriteLine($"Номер {this.number} отправляет сообщение номерам {res}");
+        }
+    }
+}
---
+++ ClassTesting/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ClassTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone iphone = new Phone();
+            iphone.number = "8-800-555-55-35";
+            iphone.model = "iphone XR";
+            iphone.weight = 520;
+            iphone.dataNumber();
+
+            Phone google = new Phone();
+            google.number = "8-777-777-55-35";
+            google.model = "google pixel 4";
+            google.weight = 570;
+            google.dataNumber();
+
+            Phone xiaomi = new Phone();
+            xiaomi.number = "8-333-543-87-98";
+            xiaomi.model = "readmi 9";
+            xiaomi.weight = 490;
+            xiaomi.dataNumber();
+
+            iphone.name = "Bob";
+            google.name = "Jill";
+            xiaomi.name = "Alex";
+
+            iphone.recieveCall(iphone);
+            google.recieveCall(google);
+            xiaomi.recieveCall(xiaomi);
+
+            Console.WriteLine(iphone.getNumber(iphone));
+            Console.WriteLine(google.getNumber(google));
+            Console.WriteLine(xiaomi.getNumber(xiaomi));
+
+            Phone sumsung = new Phone("9-888-999-88-99", "sumsung s20");
+            sumsung.dataNumber();
+
+            Phone meizu = new Phone("9-999-999-99-99", "meizu 9 pro", 460);
+            meizu.dataNumber();
+
+            Phone oppo = new Phone("8-777-888-54-33", "oppo zenFone", 625);
+            oppo.dataNumber();
+
+            iphone.recieveCall(iphone.name, iphone.number);
+
+            sumsung.sendMessage(meizu.number, iphone.number, oppo.number);
+
+            Console.ReadKey();
+        }
+    }
+}
